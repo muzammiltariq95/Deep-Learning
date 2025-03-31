a45559d0e2a4258fd0e084548ce0202b6594f82c
MuzammilTariq <muzammiltariq95@gmail.com>
2025-03-31 15:40:46 +0100

    Code completion, not finalisation

--- deep-learning.py
+++ deep-learning.py
@@ -1,11 +1,14 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from sklearn.metrics import classification_report,confusion_matrix
 
 data = pd.read_csv('bank_note_data.csv')
 
 data.head()
-
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 sns.countplot(x='Class',data=data)
 plt.show()
@@ -13,7 +16,6 @@ plt.show()
 sns.pairplot(data,hue='Class')
 plt.show()
 
-from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 scaler.fit(data.drop('Class',axis=1))
 scaled_features = scaler.fit_transform(data.drop('Class',axis=1))
@@ -23,10 +25,8 @@ df_feat.head()
 X = df_feat
 y = data['Class']
 
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-import tensorflow as tf
 df_feat.columns
 
 image_var = tf.feature_column.numeric_column("Image.Var")
@@ -52,4 +52,13 @@ final_preds  = []
 for pred in note_predictions:
     final_preds.append(pred['class_ids'][0])
 
-from sklearn.metrics import classification_report,confusion_matrix
+
+print(confusion_matrix(y_test,final_preds))
+print(classification_report(y_test,final_preds))
+
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(n_estimators=200)
+rfc.fit(X_train,y_train)
+rfc_preds = rfc.predict(X_test)
+print(classification_report(y_test,rfc_preds))
+print(confusion_matrix(y_test,rfc_preds))
